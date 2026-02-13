2669fce6222d76e00e549d9a0d9fe27848ce4f0d
Tiago Ferrer <ferrertiago@gmail.com>
2026-02-13 20:12:16 -0300

    Initialize Spring Boot project with core structure and dependencies

---
+++ src/main/java/br/com/fiap/vendasms/VendasMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.vendasms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasMsApplication.class, args);
+    }
+
+}
